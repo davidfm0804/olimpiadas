985d5ea6cd0ac6172a545211a7cb081484d13a0e
David <davidfarinamorena.guadalupe@alumnado.fundacionloyola.net>
2025-01-20 20:11:24 +0100

    Eliminar configdb.php del seguimiento de Git

--- app/config/configdb.php
+++
@@ -1,6 +0,0 @@
-<?php
-    define("SERVIDOR", "2daw.esvirgua.com"); // DATOS SERVIDOR
-    define("USUARIO", "user2daw_05");  // DATOS USUARIO BBDD
-    define("PASSWORD", 'Q1R$d23A8g.O'); // DATOS CONTRASEÑA BBDD
-    define("BBDD", "user2daw_BD2-05"); // NOMBRE BBDD
-?>
